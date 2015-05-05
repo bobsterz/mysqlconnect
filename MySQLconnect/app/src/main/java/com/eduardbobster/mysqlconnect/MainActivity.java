04d0c9409c513257ba676eeb010cb2b3d5f14da7
bobsterz <bobz8787@gmail.com>
2015-05-05 18:24:17 +0300

    Create base project

--- MySQLconnect/app/src/main/java/com/eduardbobster/mysqlconnect/MainActivity.java
+++ MySQLconnect/app/src/main/java/com/eduardbobster/mysqlconnect/MainActivity.java
@@ -2,16 +2,21 @@ package com.eduardbobster.mysqlconnect;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
     }
 
 
