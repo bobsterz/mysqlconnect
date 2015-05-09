1276ad93c3d91909593495c87005b660f442bd8e
bobsterz <bobz8787@gmail.com>
2015-05-09 21:19:36 +0300

    Version 1.0 final

--- MySQLconnect/app/src/main/java/com/eduardbobster/mysqlconnect/InsertActivity.java
+++ MySQLconnect/app/src/main/java/com/eduardbobster/mysqlconnect/InsertActivity.java
@@ -1,26 +1,28 @@
 package com.eduardbobster.mysqlconnect;
 
-        import android.app.ProgressDialog;
-        import android.os.AsyncTask;
-        import android.os.Bundle;
-        import android.support.v7.app.ActionBarActivity;
-        import android.support.v7.widget.Toolbar;
-        import android.view.View;
-        import android.widget.Button;
-        import android.widget.EditText;
-        import android.widget.Toast;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-        import org.apache.http.HttpResponse;
-        import org.apache.http.NameValuePair;
-        import org.apache.http.client.ClientProtocolException;
-        import org.apache.http.client.HttpClient;
-        import org.apache.http.client.entity.UrlEncodedFormEntity;
-        import org.apache.http.client.methods.HttpPost;
-        import org.apache.http.impl.client.DefaultHttpClient;
-        import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 
-        import java.io.IOException;
-        import java.util.ArrayList;
+import java.io.IOException;
+import java.util.ArrayList;
 
 
 public class InsertActivity extends ActionBarActivity {
@@ -35,10 +37,10 @@ public class InsertActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
 
-        etName = (EditText)findViewById(R.id.editName);
-        etPhone = (EditText)findViewById(R.id.editPhone);
-        etDesc = (EditText)findViewById(R.id.editDesc);
-        btnSendData = (Button)findViewById(R.id.btnSend);
+        etName = (EditText) findViewById(R.id.editName);
+        etPhone = (EditText) findViewById(R.id.editPhone);
+        etDesc = (EditText) findViewById(R.id.editDesc);
+        btnSendData = (Button) findViewById(R.id.btnSend);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -51,12 +53,18 @@ public class InsertActivity extends ActionBarActivity {
         });
     }
 
-    public void onClickSend (View v){
-        InsertData task1 = new InsertData();
-        task1.execute(new String[]{"http://www.bobsterz.co.ua/android/insert.php"});
+    public void onClickSend(View v) {
+        if ( !isOnline() ){
+            Toast.makeText(getApplicationContext(),
+                    "Check internet connection",Toast.LENGTH_LONG).show();
+            return;
+        } else {
+            InsertData task1 = new InsertData();
+            task1.execute(new String[]{"http://www.bobsterz.co.ua/android/insert.php"});
+        }
     }
 
-    private class InsertData extends AsyncTask<String, Void, Boolean>{
+    private class InsertData extends AsyncTask<String, Void, Boolean> {
 
         ProgressDialog dialog = new ProgressDialog(InsertActivity.this);
 
@@ -70,16 +78,16 @@ public class InsertActivity extends ActionBarActivity {
         @Override
         protected Boolean doInBackground(String... urls) {
 
-            for(String url1 : urls){
+            for (String url1 : urls) {
                 try {
                     ArrayList<NameValuePair> pairs = new ArrayList<NameValuePair>();
                     pairs.add(new BasicNameValuePair("txtName", etName.getText().toString()));
                     pairs.add(new BasicNameValuePair("txtTel", etPhone.getText().toString()));
-                    pairs.add(new BasicNameValuePair("txtMajor", etDesc.getText().toString()));
+                    pairs.add(new BasicNameValuePair("txtDesc", etDesc.getText().toString()));
 
                     HttpClient client = new DefaultHttpClient();
                     HttpPost post = new HttpPost(url1);
-                    post.setEntity(new UrlEncodedFormEntity(pairs));
+                    post.setEntity(new UrlEncodedFormEntity(pairs, "utf-8"));
                     HttpResponse response = client.execute(post);
                 } catch (ClientProtocolException e) {
                     Toast.makeText(InsertActivity.this, e.toString(), Toast.LENGTH_SHORT).show();
@@ -95,12 +103,23 @@ public class InsertActivity extends ActionBarActivity {
 
         @Override
         protected void onPostExecute(Boolean result) {
-            if(result == true){
+            if (result == true) {
                 Toast.makeText(InsertActivity.this, "Insert Success", Toast.LENGTH_SHORT).show();
-            }else {
+            } else {
                 Toast.makeText(InsertActivity.this, "Error", Toast.LENGTH_SHORT).show();
             }
             dialog.dismiss();
+        }
+    }
+
+    protected boolean isOnline() {
+        String cs = Context.CONNECTIVITY_SERVICE;
+        ConnectivityManager cm = (ConnectivityManager)
+                getSystemService(cs);
+        if (cm.getActiveNetworkInfo() == null) {
+            return false;
+        } else {
+            return true;
         }
     }
 }
--- MySQLconnect/app/src/main/java/com/eduardbobster/mysqlconnect/MainActivity.java
+++ MySQLconnect/app/src/main/java/com/eduardbobster/mysqlconnect/MainActivity.java
@@ -1,41 +1,78 @@
 package com.eduardbobster.mysqlconnect;
 
+/**
+ * Activity download records in ListView from MySQL.
+ *
+ * This activity showing records from database using JSON, PHP.
+ *
+ * @author Eduard Bobsterz
+ * @version 1.0
+ */
+
+
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
 
 
 public class MainActivity extends ActionBarActivity {
 
-    ListView list1;
+    ListView listView1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listView1 = (ListView)findViewById(R.id.listView);
 
+        //Стандартный тулбар из библиотеки appcompat-v7:21.0.3
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+
+        //При запуске активити данный метод обновляет наш listView1
+        onClickUpdate();
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
+    //обработчик события на вью элементах которые закреплены на тулбаре :D
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_update:
-
+                onClickUpdate();
                 return true;
             case R.id.action_add:
                 Intent intent = new Intent(getApplicationContext(), InsertActivity.class);
@@ -46,16 +83,112 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-//    public void onClickUpdate(View arg0){
-//        ReadData task1 = new ReadData();
-//        task1.execute(new String[]{});
-//    }
-//
-//    private class ReadData extends AsyncTask<String, Void, Boolean>{
-//
-//        @Override
-//        protected Boolean doInBackground(String... urls) {
-//            return null;
-//        }
+    //данный метот получает записи из базы данных для нашего listView1
+    //создаеться экземпляр класса где все и реализовано.
+    //также реализована проверка через if есть ли доступ к интернету
+    public void onClickUpdate() {
+        if ( !isOnline() ){
+            Toast.makeText(getApplicationContext(),
+                    "Check internet connection",Toast.LENGTH_LONG).show();
+            return;
+        } else {
+            ReadData task1 = new ReadData();
+            task1.execute(new String[]{"http://bobsterz.co.ua/android/readjson.php"});
+        }
     }
 
+    //В данном классе испотльзовано наследие от AsyncTask и задействованы
+    //переопределение методов onPreExecute(), doInBackground(), onPostExecute.
+    private class ReadData extends AsyncTask<String, Void, Boolean> {
+        ProgressDialog dialog = new ProgressDialog(MainActivity.this);
+
+        //данный метод срабатывает первым и ждет выполнения метода doInBackground().
+        @Override
+        protected void onPreExecute() {
+            dialog.setMessage("Reading Data...");
+            dialog.show();
+            super.onPreExecute();
+        }
+
+        String text = "";
+        ArrayList<String> list1;
+
+
+        //метод получения данных
+        @Override
+        protected Boolean doInBackground(String... urls) {
+            //создаем строку для входного потока
+            InputStream is1;
+            //подключаемся и читаем данные которые пришли
+            for (String url1 : urls) {
+                try {
+                    HttpClient client = new DefaultHttpClient();
+                    HttpPost post = new HttpPost(url1);
+                    HttpResponse response = client.execute(post);
+                    is1 = response.getEntity().getContent();
+
+                } catch (ClientProtocolException e) {
+                    Toast.makeText(MainActivity.this, e.toString(), Toast.LENGTH_SHORT).show();
+                    return false;
+                } catch (IOException e) {
+                    Toast.makeText(MainActivity.this, e.toString(), Toast.LENGTH_SHORT).show();
+                    return false;
+                }
+
+                //конверитуем наши данные в текст
+                BufferedReader reader;
+                try {
+                    reader = new BufferedReader(new InputStreamReader(is1, "utf-8"), 8);
+                    String line = null;
+                    while ((line = reader.readLine()) != null){
+                        text += line + "\n";
+                    }
+                    is1.close();
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                //Конвертируем текст в формат JSON в массив list1
+                list1 = new ArrayList<String>();
+                try {
+                    JSONArray json = new JSONArray(text);
+                    for (int i = 0; i<json.length(); i++){
+                        JSONObject jsonData = json.getJSONObject(i);
+                        list1.add(jsonData.getString("st_name"));
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+            //результат тру
+            return true;
+        }
+
+        //завершение
+        //и если результат тру, создаем ArrayAdapter и записываем данные в listView1.
+        @Override
+        protected void onPostExecute(Boolean result) {
+            if (result == true) {
+                ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, list1);
+                listView1.setAdapter(adapter1);
+            } else {
+                Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
+            }
+            dialog.dismiss();
+        }
+    }
+
+    //проверка подключен ли интернет на устройстве
+    protected boolean isOnline() {
+        String cs = Context.CONNECTIVITY_SERVICE;
+        ConnectivityManager cm = (ConnectivityManager)
+                getSystemService(cs);
+        if (cm.getActiveNetworkInfo() == null) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
